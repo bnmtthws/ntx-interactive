240694eccfc095863a63ccdcf957b521fdf17022
ben matthews <bmatthews@rockefeller.edu>
2016-09-02 11:32:23 -0400

    for initial release

--- ntx-interactive-plottingFunctions.R
+++ ntx-interactive-plottingFunctions.R
@@ -10,12 +10,15 @@ plotData <- function(genes, tissues) {
   ### classify data by state, in addition to individual libraries
   colnames(tpm_subset.melt)[5:6] <- c("Library.ID","Transcripts.Per.Million")
   
-  tpm_subset.melt <- merge(lib_info[,c("Library.ID","tissue","state","sex")], tpm_subset.melt,by="Library.ID")
+  tpm_subset.melt <- merge(lib_info[,c("Library.ID","tissue","state","sex","condition")], tpm_subset.melt,by="Library.ID")
+
+  tpm_subset.melt$condition <- revalue(tpm_subset.melt$condition, c("SF"="Sugarfed_0h", "BF"="Bloodfed_48h","O"="Gravid_96h","male"="Male"))  
+  tpm_subset.melt$condition <- factor(tpm_subset.melt$condition, c("Sugarfed_0h", "Bloodfed_48h", "Gravid_96h", "male"))
   
   ### generate plot using ggplot2
   final_plot <- ggplot(data=tpm_subset.melt[tpm_subset.melt$tissue %in% tissues,],
                        aes(x=Vectorbase.Identifier,y=Transcripts.Per.Million,
-                           group=state,colour=state)) + 
+                           group=condition,colour=condition)) + 
     geom_point(position=position_dodge(width = .5)) +
     facet_grid(sex ~ tissue)
   final_plot <- final_plot + ggtitle("expression profile")
--- ntx-interactive.R
+++ ntx-interactive.R
@@ -1,22 +1,25 @@
 ### import the required libraries
 library(ggplot2)
-library(gdata)
 library(reshape2)
+library(plyr)
 
-### set your working directory
+### set your working directory to the github repository home directory
 setwd('~/bioinfo/github/ntx-interactive/')
 
 ### load the data behind the scenes
 source('ntx-interactive-loadData.R')
 source('ntx-interactive-plottingFunctions.R')
 
+### examplePlot
 ### define our gene(s) of interest
 genes = c('AAEL010779','AAEL000582')
 tissues = c('brain','antenna')
 
-### examplePlot
+### call function to generate plot
+ppkBrainAntenna <- plotData(genes,tissues)
+show(ppkBrainAntenna)
 
-ppkBrain <- plotData(genes,tissues)
-
+### example of interactive calling from the command line
+elavBrainAntenna <- plotData("AAEL008164",c("brain","antenna"))
 
 
